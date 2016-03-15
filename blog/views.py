2670f4fe11d081c4634853a9dd685c2f05bf1d75
fdemircan <demircanfetiye@gmail.com>
2016-03-15 18:01:58 +0200

    copy paste yapmak kotudur

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
